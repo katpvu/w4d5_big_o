a679e4fd574b37d93a94752e4766dea71ce23bf3
Leah Seyoum <leahseyoum@gmail.com>
2023-02-24 14:39:53 -0800

    subsets

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/execution_time_diffs.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/execution_time_diffs.rb
@@ -42,28 +42,43 @@ def bad_largest_contiguous_subsum(array)
     subsets.map {|subset| subset.sum }.max
 end
 
-list = [5, 3, -7]
-p bad_largest_contiguous_subsum(list) # => 8
+# list = [5, 3, -7]
+# p bad_largest_contiguous_subsum(list) # => 8
 
-list_1 = [2, 3, -6, 7, -6, 7]
-p bad_largest_contiguous_subsum(list_1) # => 8 (from [7, -6, 7])
+# list_1 = [2, 3, -6, 7, -6, 7]
+# p bad_largest_contiguous_subsum(list_1) # => 8 (from [7, -6, 7])
 
-list_2 = [-5, -1, -3]
-p bad_largest_contiguous_subsum(list_2) # => -1 (from [-1])
+# list_2 = [-5, -1, -3]
+# p bad_largest_contiguous_subsum(list_2) # => -1 (from [-1])
 
 #phase II:
 
 def good_largest_contiguous_subsum(array)
-    bank = {5}
-    largest_sum = 5
-    current_sum = -1
-    largest_sum = current_sum if largest_sum < current_sum
-    array.each_with_index do |ele, i|
-        
+    largest_sum = array.first
+    current_sum = array.first
+    
+    (1...array.length).each do |i|
+        if current_sum < 0
+            current_sum = 0
+        end
+
+        current_sum += array[i]
+
+        if current_sum > largest_sum
+            largest_sum = current_sum
+        end
     end
+
+    largest_sum
 end
 
-
 list = [5, 3, -7]
-5 
-5 + 3
+p good_largest_contiguous_subsum(list) # => 8
+
+list_1 = [2, 3, -6, 7, -6, 7]
+p good_largest_contiguous_subsum(list_1) # => 8 (from [7, -6, 7])
+
+list_2 = [-5, -1, -3]
+p good_largest_contiguous_subsum(list_2) # => -1 (from [-1])
+
+
