2ddc079327af5a9ca7055774dc73f09223d3a2d8
Kathy Vu <vu.kathy98@gmail.com>
2023-02-24 13:28:50 -0800

    Add my_min and largest_con_subsum methods

---
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/execution_time_diffs.rb
@@ -0,0 +1,69 @@
+#phase I: quadratic O(n^2)
+def my_bad_min(array)
+    smallest = array.first
+    array.each_with_index do |ele, i| # 0 
+        array.each_with_index do |ele2, i2| # 3
+            if i2 > i && ele < ele2
+                smallest = ele if ele < smallest
+            end
+
+            # if i2 != i 
+            #     next if ele > ele2
+            #     return ele if i2 == array.length - 1 && ele < ele2
+            # end
+        end
+    end
+    smallest
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_bad_min(list) # => 5
+
+
+#phase II: linear O(n)
+def my_good_min(array)
+    smallest = array.first
+    array.each { |ele| smallest = ele if ele < smallest }
+    smallest
+end
+
+# p my_good_min(list) # => 5
+
+
+#phase I: quadratic O(n^2)
+def bad_largest_contiguous_subsum(array)
+    subsets = []
+    array.each_with_index do |ele, i|
+        (0..i).each do |i2|
+            subsets << array[i2..i]
+        end
+    end
+
+    subsets.map {|subset| subset.sum }.max
+end
+
+list = [5, 3, -7]
+p bad_largest_contiguous_subsum(list) # => 8
+
+list_1 = [2, 3, -6, 7, -6, 7]
+p bad_largest_contiguous_subsum(list_1) # => 8 (from [7, -6, 7])
+
+list_2 = [-5, -1, -3]
+p bad_largest_contiguous_subsum(list_2) # => -1 (from [-1])
+
+#phase II:
+
+def good_largest_contiguous_subsum(array)
+    bank = {5}
+    largest_sum = 5
+    current_sum = -1
+    largest_sum = current_sum if largest_sum < current_sum
+    array.each_with_index do |ele, i|
+        
+    end
+end
+
+
+list = [5, 3, -7]
+5 
+5 + 3
